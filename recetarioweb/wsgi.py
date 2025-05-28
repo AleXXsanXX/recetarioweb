3013d4e5e8054e2d162732063ac728e36058dbb8
Alejandro <celtico8000@gmail.com>
2025-05-27 23:02:32 -0600

    Agrego wsgi.py que carga settings de development.ini

--- recetarioweb/wsgi.py
+++ recetarioweb/wsgi.py
@@ -1,3 +1,9 @@
+import os
 from recetarioweb import main
+from pyramid.paster import get_appsettings
 
-app = main({})
+ini_path = os.path.join(os.path.dirname(__file__), '..', 'development.ini')
+
+settings = get_appsettings(ini_path)
+
+app = main({}, **settings)
