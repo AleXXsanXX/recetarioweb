6b8ed04921bfcc5eb77ba42ff5ece7d326424c30
Alejandro <celtico8000@gmail.com>
2025-05-27 19:49:17 -0600

    Actualizo setup.py y archivos para corregir errores y despliegue

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'zope.sqlalchemy',
+    'transaction',
+    'waitress',
+    'alembic',
+    # cualquier otra dependencia que tuvieras
+]
+
 setup(
     name='recetarioweb',
-    version='0.0',
+    version='0.1',
+    description='Recetario Web project',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'pyramid',
-        'SQLAlchemy',
-        'zope.sqlalchemy',
-        'pyramid_jinja2',
-        'alembic',
-        'waitress',
-        'bcrypt'
-    ],
+    install_requires=requires,
     entry_points={
         'paste.app_factory': [
             'main = recetarioweb:main',
         ],
     },
 )
-
